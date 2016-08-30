81bca06e9b83fd1e305d4aa4f32d0aae564a01d1
Jim Maddock <skimaddster@gmail.com>
2016-08-30 09:26:29 -0500

    downloader makes single query to get diffs

--- src/download_wiki_page.py
+++ src/download_wiki_page.py
@@ -8,50 +8,86 @@ from datetime import datetime
 #import config
 #import utils
 
-RVLIMIT = 'max'
+# set RVLIMIT to 1 to get all diffs
+RVLIMIT = '1'
 
-def get_revisions_from_web(json_dump=[]):
+def get_revisions_from_web(title,json_dump=[]):
+    # set initial params for API query
+    rvlimit = RVLIMIT
     base_url = r'https://en.wikipedia.org/w/api.php'
     params = {
         'action':'query',
         'prop':'revisions',
-        'titles':'Talk:November 2015 Paris attacks',
+        'titles':title,
         'rvprop':'ids|timestamp|comment|user|sha1|content',
-        'rvlimit':RVLIMIT,
-        'rvdiffto':'prev',
+        'rvlimit':rvlimit,
+        'rvdiffto':'next',
         'rvdir':'newer',
         'format':'json'
     }
-    lastContinue = None
+    # if not passed a json dump, start with the oldest edit
+    # else start with the last edit in the json dump
+    if len(json_dump) == 0:
+        next_rev = None
+    else:
+        next_rev = json_dump[-1]['revisions'][0]['diff']['to']
+    download_count = 0
     while True:
-        if lastContinue:
-            params.update({'rvcontinue':lastContinue})
-        r = requests.get(base_url, params=params).json()
-        if 'error' in r:
-            print(r['error'])
-            return None
-        if 'warnings' in r:
-            print(r['warnings'])
-        if 'query' in r:
-            page_id = list(r['query']['pages'].keys())[0]
-            json_dump.append({
-                'query':r['query']['pages'][page_id],
-                ' batch_download_date':str(datetime.utcnow())
-            })
-            #df = df.append(traverse_query(r['query']['categorymembers']))
-            #utils.log('processed {0} bot names'.format(len(df)))
-        if 'continue' not in r:
+        # try/except keyboardinterrupt to dump all collected data
+        try:
+            # updated the starting revision ID for the query
+            if next_rev:
+                params.update({
+                    'rvstartid':next_rev,
+                    'rvlimit':rvlimit
+                })
+            # make API request
+            r = requests.get(base_url, params=params).json()
+            # handle server errors and return collected data
+            if 'error' in r:
+                print(r['error'])
+                return json_dump
+            # handle server warnings and continue
+            if 'warnings' in r:
+                print(r['warnings'])
+            # get the query result
+            if 'query' in r:
+                page_id = list(r['query']['pages'].keys())[0]
+                # append the query result to the output json dump with the download date
+                json_dump.append({
+                    'query':r['query']['pages'][page_id],
+                    'batch_download_date':str(datetime.utcnow())
+                })
+                # try to update the next revision ID
+                # if query does not have a next ID, increase query limit by 1
+                try:
+                    next_rev = r['query']['pages'][page_id]['revisions'][-1]['diff']['to']
+                    rvlimit = 1
+                except KeyError as e:
+                    print(e)
+                    print(r['query'])
+                    rvlimit += 1
+                download_count += 1
+            # log number of queries performed to stdout
+            if download_count % 100 == 0:
+                print('downloaded {0} revisions'.format(download_count))
+            # if no more revision, return output json dump
+            if 'next_rev' == 0:
+                print('query_complete')
+                return json_dump
+        # handle keyboardinterrupt exception, return collected data
+        except KeyboardInterrupt as e:
+            print(e)
             return json_dump
-        lastContinue = r['continue']['rvcontinue']
 
-def create_json_dump_from_web(outfile_path,infile_path=None):
+def create_json_dump_from_web(outfile_path,title,infile_path=None):
     if not infile_path:
         json_dump = []
     else:
         infile = open(infile_path,'r')
         json_dump = json.load(infile)
         infile.close()
-    json_dump = get_revisions_from_web(json_dump)
+    json_dump = get_revisions_from_web(title=title,json_dump=json_dump)
     outfile = open(outfile_path,'w')
     json.dump(json_dump,outfile)
 
@@ -63,9 +99,11 @@ def main():
     parser.add_argument('-o','--outfile',
                         help='output json dump file path')
     parser.add_argument('-t','--title',
+                        default='Talk:November 2015 Paris attacks',
                         help='the title of the wikipedia page to download')
     args = parser.parse_args()
     create_json_dump_from_web(outfile_path=args.outfile,
+                              title=args.title,
                               infile_path=args.infile)
 
 if __name__ == "__main__":
--- src/parse_json_dump.py
+++ src/parse_json_dump.py
@@ -5,43 +5,65 @@ import argparse
 import json
 import pandas as pd
 from bs4 import BeautifulSoup
+import mwparserfromhell as mw
 
 def traverse_query(query):
     df = pd.DataFrame()
-    page_list = query['pages']
-    for page in page_list:
-        title = page_list[page]['title']
-        page_id = page_list[page]['pageid']
-        revision_list = page_list[page]['revisions']
-        for rev in revision_list:
-            rev_id = rev['revid']
-            parent_id = rev['parentid']
-            ts = pd.to_datetime(rev['timestamp'],format="%Y-%m-%d %H:%M:%S")
-            comment = rev['comment']
+    title = query['title']
+    page_id = query['pageid']
+    revision_list = query['revisions']
+    for rev in revision_list:
+        rev_id = rev['revid']
+        parent_id = rev['parentid']
+        ts = pd.to_datetime(rev['timestamp'],format="%Y-%m-%d %H:%M:%S")
+        comment = rev['comment']
+        if 'user' in rev:
             user = rev['user']
+        else:
+            user = ''
+        if 'diff' in rev:
             if 'from' in rev['diff']:
                 text = parse_revision_xml(rev['diff']['*'])
             else:
                 text = rev['*']
-            df = df.append(pd.DataFrame([{
-                'title':title,
-                'page_id':page_id,
-                'rev_id':rev_id,
-                'parent_rev_id':parent_id,
-                'ts':ts,
-                'user_comment':comment,
-                'user':user,
-                'text':text
-            }]))
+            stripped_text = strip_wikicode(text)
+        else:
+            text = ''
+            stripped_text = ''
+            
+        df = df.append(pd.DataFrame([{
+            'title':title,
+            'page_id':page_id,
+            'rev_id':rev_id,
+            'parent_rev_id':parent_id,
+            'ts':ts,
+            'user_comment':comment,
+            'user':user,
+            'text':text,
+            'stripped_text':stripped_text
+        }]))
     return df
 
+def strip_wikicode(text):
+    # get all wikicode templates
+    template_list = mw.parse(text).filter_templates()
+    stripped_text = text
+    # if there are templates remove each template from the text string
+    if template_list:
+        for t in template_list:
+            stripped_text = stripped_text.replace(str(t),'')
+    # strip all endline characters
+    # add a space at the beginning for google sheets compatibility
+    stripped_text = ' {0}'.format(stripped_text.strip('\n'))
+    return stripped_text
+
 def parse_revision_xml(xml):
-    text = ''
+    diff_text = ''
     parsed_xml = BeautifulSoup(xml,'html.parser')
     added_line_list = parsed_xml.select('.diff-addedline')
     for line in added_line_list:
-        text = '{0}{1}\n'.format(text,line.text)
-    return text
+        diff_text = '{0}{1}\n'.format(diff_text,line.text)
+    return diff_text
 
 def main():
     description = 'parse a json dump containing the revision history of a wikipage and return a csv of diffs for coding'
@@ -53,13 +75,13 @@ def main():
                         help='output csv file path')
     args = parser.parse_args()
     df = pd.DataFrame()
-    for infile_path in args.infile_path:
+    for infile_path in args.infile:
         infile = open(infile_path,'r')
         json_dump = json.load(infile)
         infile.close()
         for query in json_dump:
-            df = df.append(traverse_query(query))
-    df.to_csv(args.outfile_path,na_rep='NaN',columns=columns,encoding='utf-8')
+            df = df.append(traverse_query(query['query']))
+    df.to_csv(args.outfile,na_rep='NaN',encoding='utf-8')
             
 if __name__ == "__main__":
     main()
